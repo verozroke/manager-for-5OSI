618313594514d47b39c2bdd44ae693c4abca3b9e
Askhat <73778760+verozroke@users.noreply.github.com>
2023-03-26 14:24:13 +0600

    bruh

--- server/src/routers/admin.js
+++ server/src/routers/admin.js
@@ -1,10 +1,12 @@
 const updateNews = require("../handlers/admin/updateNews");
 const updateProposal = require("../handlers/admin/updateProposal");
+const login  = require("../handlers/admin/login");
 
 const router = require("express").Router();
 
 router.put("/proposal/:proposalId", updateProposal);
 router.put("/news/:rescomId", updateNews);
+router.post("/login", login);
 
 
 
